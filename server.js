9b5c23019b06dc55c90db438f6316a23ed378601
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-11-26 01:47:14 +0500

    Delete package-lock.json

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-// Faqat API / WebSocket server
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// index.html ni yuborish
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Socket.IO
 io.on("connection", (socket) => {
   console.log("Yangi user:", socket.id);
-
   socket.on("chat message", (msg) => {
     io.emit("chat message", { id: socket.id, text: msg });
   });
-
   socket.on("disconnect", () => {
     console.log("Foydalanuvchi uzuldi:", socket.id);
   });
