34e03b6c4ef97c8da2056eadeef0905bf63b921d
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-11-28 22:16:48 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// index.html ni yuborish
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(__dirname + "/index.html");
 });
 
-// Socket.IO
 io.on("connection", (socket) => {
   console.log("Yangi user:", socket.id);
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", { id: socket.id, text: msg });
+
+  socket.on("chat message", (data) => {
+    io.emit("chat message", { id: socket.id, ...data });
   });
+
   socket.on("disconnect", () => {
     console.log("Foydalanuvchi uzuldi:", socket.id);
   });
 });
 
-server.listen(3000, () => console.log("Server ishga tushdi: 3000"));
+server.listen(3000, () => console.log("Server 3000 portda ishlayapti"));
